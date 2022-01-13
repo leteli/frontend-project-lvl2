0ccbfba3ff35b97899159787f35048a7ac031013
Nastia Kartseva <danetzachem@gmail.com>
2022-01-13 12:09:41 +0300

    update gendiff cli program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/gendiff-code.js';
+import * as fs from 'fs';
+import _ from 'lodash';
+
 const program = new Command();
 
 program
@@ -8,6 +12,13 @@ program
     .argument('<filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format');
+    .option('-f, --format [type]', 'output format')
+    .action((filepath1, filepath2) => {
+      const data1 = fs.readFileSync(filepath1, 'utf8');
+      const data2 = fs.readFileSync(filepath2, 'utf8');
+      const obj1 = JSON.parse(data1);
+      const obj2 = JSON.parse(data2);
+      console.log(genDiff(obj1, obj2));
+    });
 
 program.parse();
