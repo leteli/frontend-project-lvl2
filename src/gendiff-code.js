f77d21543620108457d97089ec9d68b7969ae4f9
Nastia Kartseva <danetzachem@gmail.com>
2022-01-22 19:44:15 +0300

    update gendiff code

--- src/gendiff-code.js
+++ src/gendiff-code.js
@@ -8,22 +8,35 @@ export const getPath = (filename) => path.resolve('__fixtures__', filename);
 export const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
 
 export const genDiff = (filepath1, filepath2) => {
-  const obj1 = parser(readFile(filepath1), filepath1);
-  const obj2 = parser(readFile(filepath2), filepath2);
-  const keys = Object.keys(obj1).concat(Object.keys(obj2));
-  const sortedKeys = _.sortBy(keys);
-  const result = sortedKeys.map((key) => {
-    if (obj1[key] === obj2[key]) {
-      return `    ${key}: ${obj1[key]}`;
-    }
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-    }
-    if (_.has(obj1, key)) {
-      return `  - ${key}: ${obj1[key]}`;
-    }
-    return `  + ${key}: ${obj2[key]}`;
-  });
-  const entries = _.sortedUniq(result);
-  return `{\n${entries.join('\n')}\n}`;
+  const parsedObj1 = parser(readFile(filepath1), filepath1);
+  const parsedObj2 = parser(readFile(filepath2), filepath2);
+  const buildDiffTree = (obj1, obj2) => {
+    const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
+    return keys.flatMap((key) => {
+      if (!_.has(obj2, key)) {
+        return {
+          name: key, type: 'deleted', value: _.cloneDeep(obj1[key]),
+        };
+      } if (!_.has(obj1, key)) {
+        return {
+          name: key, type: 'added', value: _.cloneDeep(obj2[key]),
+        };
+      } if (obj1[key] === obj2[key]) {
+        return {
+          name: key, type: 'unchanged', value: _.cloneDeep(obj1[key]),
+        };
+      }
+      if (!(_.isObject(obj1[key]) && _.isObject(obj2[key]))) {
+        return [{
+          name: key, type: 'deleted', value: _.cloneDeep(obj1[key]),
+        }, {
+          name: key, type: 'added', value: _.cloneDeep(obj2[key]),
+        }];
+      }
+      return {
+        name: key, value: 'nested', children: buildDiffTree(obj1[key], obj2[key]),
+      };
+    });
+  };
+  return buildDiffTree(parsedObj1, parsedObj2);
 };
