c75d0657536b3b9983da9794aa99f6288e1ca001
Nastia Kartseva <danetzachem@gmail.com>
2022-01-24 14:05:29 +0300

    update bin

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,6 @@
 
 import { Command } from 'commander/esm.mjs';
 import { getPath, genDiff } from '../src/gendiff-code.js';
-import stylish from '../src/stylish.js';
 
 const program = new Command();
 
@@ -11,9 +10,9 @@ program
   .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format', stylish)
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(stylish(genDiff(getPath(filepath1), getPath(filepath2))));
+    console.log(genDiff(getPath(filepath1), getPath(filepath2), program.opts().format));
   });
 
 program.parse();
