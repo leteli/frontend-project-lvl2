2687052306b96936ce836f420074db869bd08aad
Nastia Kartseva <danetzachem@gmail.com>
2022-01-24 14:05:44 +0300

    add new test

--- __tests__/gendiff-code.test.js
+++ __tests__/gendiff-code.test.js
@@ -3,7 +3,6 @@ import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { genDiff } from '../src/gendiff-code.js';
-import stylish from '../src/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,14 +10,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-test('formatted diff for nested structures 2 json', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
-  expect(stylish(genDiff(filepath1, filepath2))).toEqual(readFile('diff1.txt'));
-});
-
-test('formatted diff for nested structures json yml', () => {
+test('stylish diff for nested structures json yml', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.yml');
-  expect(stylish(genDiff(filepath1, filepath2))).toEqual(readFile('diff1.txt'));
+  expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(readFile('stylish-diff.txt'));
+});
+
+test('plain diff', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.json');
+  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(readFile('plain-diff.txt'));
 });
