010cbd863e26ef5e9098a8fbb3c67140977ed9ab
Nastia Kartseva <danetzachem@gmail.com>
2022-01-24 15:08:25 +0300

    update chooseFormatter

--- formatters/index.js
+++ formatters/index.js
@@ -1,5 +1,6 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 // eslint-disable-next-line consistent-return
 const chooseFormatter = (data, formatName) => {
@@ -7,6 +8,9 @@ const chooseFormatter = (data, formatName) => {
     return stylish(data);
   } if (formatName === 'plain') {
     return plain(data);
+  }
+  if (formatName === 'json') {
+    return json(data);
   }
 };
 export default chooseFormatter;
