87dd710d8d37751f8a094f30c741a86c30387ddf
Nastia Kartseva <danetzachem@gmail.com>
2022-01-24 15:50:22 +0300

    refactor stylish

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -11,6 +11,15 @@ const stylish = (data, replacer = '  ', spacesCount = 1) => {
     const indentSize = spacesCount * depth;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    // eslint-disable-next-line consistent-return
+    const getTypeSign = (type) => {
+      if (type === 'deleted') {
+        return '- ';
+      } if (type === 'added') {
+        return '+ ';
+      }
+      return '  ';
+    };
     if (_.isObject(tree) && !Array.isArray(tree)) {
       const lines = Object
         .entries(tree)
@@ -18,32 +27,13 @@ const stylish = (data, replacer = '  ', spacesCount = 1) => {
       return ['{', ...lines, `${bracketIndent}}`].join('\n');
     }
     const result = tree.map((node) => {
-      let typeSign;
-      switch (node.type) {
-        case 'deleted':
-        case 'changed-deleted':
-          typeSign = '- ';
-          break;
-        case 'added':
-        case 'changed-added':
-          typeSign = '+ ';
-          break;
-        case 'unchanged':
-          typeSign = '  ';
-          break;
-        case 'changed':
-          break;
-        default:
-          typeSign = '  ';
-          break;
-      }
       if (node.type === 'changed') {
         return `${currentIndent}- ${node.name}: ${iter(node.valueBefore, depth + 2)}\n${currentIndent}+ ${node.name}: ${iter(node.valueAfter, depth + 2)}`;
       }
       if (node.value !== 'nested') {
-        return `${currentIndent}${typeSign}${node.name}: ${iter(node.value, depth + 2)}`;
+        return `${currentIndent}${getTypeSign(node.type)}${node.name}: ${iter(node.value, depth + 2)}`;
       }
-      return `${currentIndent}${typeSign}${node.name}: ${iter(node.children, (depth + 2))}`;
+      return `${currentIndent}${getTypeSign(node.type)}${node.name}: ${iter(node.children, (depth + 2))}`;
     });
     return ['{', ...result, `${bracketIndent}}`].join('\n');
   };
