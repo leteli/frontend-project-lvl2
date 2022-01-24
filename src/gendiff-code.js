5b19c5893b4e1a027b36014ea06d51d01e6780a4
Nastia Kartseva <danetzachem@gmail.com>
2022-01-24 14:03:50 +0300

    update gendiff code

--- src/gendiff-code.js
+++ src/gendiff-code.js
@@ -2,12 +2,13 @@ import * as fs from 'fs';
 import * as path from 'path';
 import _ from 'lodash';
 import parser from './parsers.js';
+import chooseFormatter from '../formatters/index.js';
 
 export const getPath = (filename) => path.resolve('__fixtures__', filename);
 
 export const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
 
-export const genDiff = (filepath1, filepath2) => {
+export const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const parsedObj1 = parser(readFile(filepath1), filepath1);
   const parsedObj2 = parser(readFile(filepath2), filepath2);
   const buildDiffTree = (obj1, obj2) => {
@@ -27,16 +28,15 @@ export const genDiff = (filepath1, filepath2) => {
         };
       }
       if (!(_.isObject(obj1[key]) && _.isObject(obj2[key]))) {
-        return [{
-          name: key, type: 'deleted', value: _.cloneDeep(obj1[key]),
-        }, {
-          name: key, type: 'added', value: _.cloneDeep(obj2[key]),
-        }];
+        return {
+          name: key, type: 'changed', valueBefore: _.cloneDeep(obj1[key]), valueAfter: _.cloneDeep(obj2[key]),
+        };
       }
       return {
         name: key, value: 'nested', children: buildDiffTree(obj1[key], obj2[key]),
       };
     });
   };
-  return buildDiffTree(parsedObj1, parsedObj2);
+  const tree = buildDiffTree(parsedObj1, parsedObj2);
+  return chooseFormatter(tree, formatName);
 };
