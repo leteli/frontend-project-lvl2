a57d3c161e972b7bc7e3ad584b27319b008cddff
Nastia Kartseva <danetzachem@gmail.com>
2022-01-25 12:54:37 +0300

    update parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,13 @@
 import yaml from 'js-yaml';
+import * as fs from 'fs';
 import * as path from 'path';
 
-const parser = (data, filepath) => {
+const getPath = (filename) => path.resolve('__fixtures__', filename);
+
+const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
+
+const parser = (filepath) => {
+  const data = readFile(getPath(filepath));
   const extension = path.extname(filepath);
   if (extension === '.json') {
     return JSON.parse(data);
