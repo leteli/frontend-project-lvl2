ce9cc0d05d2b18ec5f36269163529ba6a96d80dd
Nastia Kartseva <danetzachem@gmail.com>
2022-01-27 20:50:48 +0300

    refactor gendiff-code

--- src/gendiff-code.js
+++ src/gendiff-code.js
@@ -1,37 +1,20 @@
-import _ from 'lodash';
+import * as fs from 'fs';
+import * as path from 'path';
 import parser from './parsers.js';
-import chooseFormatter from '../formatters/index.js';
+import chooseFormatter from './formatters/index.js';
+import buildDiffTree from './buildDiffTree.js';
+
+const getPath = (filename) => path.resolve('__fixtures__', filename);
+
+const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const parsedObj1 = parser(filepath1);
-  const parsedObj2 = parser(filepath2);
-  const buildDiffTree = (obj1, obj2) => {
-    const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-    const sortedKeys = _.sortBy(keys);
-    return sortedKeys.flatMap((key) => {
-      if (!_.has(obj2, key)) {
-        return {
-          name: key, type: 'deleted', value: _.cloneDeep(obj1[key]),
-        };
-      } if (!_.has(obj1, key)) {
-        return {
-          name: key, type: 'added', value: _.cloneDeep(obj2[key]),
-        };
-      } if (obj1[key] === obj2[key]) {
-        return {
-          name: key, type: 'unchanged', value: _.cloneDeep(obj1[key]),
-        };
-      }
-      if (!(_.isObject(obj1[key]) && _.isObject(obj2[key]))) {
-        return {
-          name: key, type: 'changed', valueBefore: _.cloneDeep(obj1[key]), valueAfter: _.cloneDeep(obj2[key]),
-        };
-      }
-      return {
-        name: key, type: 'nested', children: buildDiffTree(obj1[key], obj2[key]),
-      };
-    });
-  };
+  const data1 = readFile(getPath(filepath1));
+  const data2 = readFile(getPath(filepath2));
+  const ext1 = path.extname(filepath1);
+  const ext2 = path.extname(filepath2);
+  const parsedObj1 = parser(data1, ext1);
+  const parsedObj2 = parser(data2, ext2);
   const tree = buildDiffTree(parsedObj1, parsedObj2);
   return chooseFormatter(tree, formatName);
 };
