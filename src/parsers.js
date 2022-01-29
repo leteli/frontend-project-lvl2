a34db7ec0d74cc74c0033a58b18dec52f925eac1
Nastia Kartseva <danetzachem@gmail.com>
2022-01-29 19:07:04 +0300

    edit parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,14 @@
 import yaml from 'js-yaml';
 
-const parser = (data, extension) => {
-  switch (extension) {
-    case '.json':
+const parser = (data, format) => {
+  switch (format) {
+    case 'json':
       return JSON.parse(data);
-    case '.yml':
-    case '.yaml':
+    case 'yml':
+    case 'yaml':
       return yaml.load(data);
     default:
-      throw new Error(`Unknown extension!: ${extension}`);
+      throw new Error(`Unknown data format!: ${format}`);
   }
 };
 export default parser;
