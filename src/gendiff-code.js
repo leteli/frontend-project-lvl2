5562c7b8f31ecaf6419cd3b93d0191d0e4b216f6
Nastia Kartseva <danetzachem@gmail.com>
2022-01-29 19:01:40 +0300

    edit import from diff-tree

--- src/gendiff-code.js
+++ src/gendiff-code.js
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import parser from './parsers.js';
 import chooseFormatter from './formatters/index.js';
-import buildDiffTree from './buildDiffTree.js';
+import buildDiffTree from './diff-tree.js';
 
 const getPath = (filename) => path.resolve('__fixtures__', filename);
 
@@ -11,10 +11,10 @@ const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = readFile(getPath(filepath1));
   const data2 = readFile(getPath(filepath2));
-  const ext1 = path.extname(filepath1);
-  const ext2 = path.extname(filepath2);
-  const parsedObj1 = parser(data1, ext1);
-  const parsedObj2 = parser(data2, ext2);
+  const format1 = path.extname(filepath1).slice(1);
+  const format2 = path.extname(filepath2).slice(1);
+  const parsedObj1 = parser(data1, format1);
+  const parsedObj2 = parser(data2, format2);
   const tree = buildDiffTree(parsedObj1, parsedObj2);
   return chooseFormatter(tree, formatName);
 };
